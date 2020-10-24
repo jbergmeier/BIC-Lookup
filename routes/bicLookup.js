721757c45db62168495028cc9a5b32642e7ab2af
Joern Bergmeier <joern.bergmeier@arvato.com>
2020-10-24 11:07:34 +0200

    clean up code

--- routes/bicLookup.js
+++ routes/bicLookup.js
@@ -11,54 +11,58 @@ router.get('/:iban', function (req, res, next) {
     iban = iban.split(" "). join("")
     let bankCode = ''
     let bankSearchResult = ''
-console.log(iban)
-    
 
     try {
         switch (countryCode){
             
             // DE - Germany
             case 'DE':
-              if(ibanLength != 22) res.json({message: 'IBAN Length is not correct', responseCode: '200.101'})
-      
-              // Get BankCode (Country Specific)
-              bankCode = iban.substring(4,12)
-              bankSearchResult = bankCodes.filter(it => it.bankCode === bankCode)
-              if(bankSearchResult == '') res.json({message: 'BIC not found for used IBAN', iban, bankCode, responseCode: '200.102'})
-                console.log(bankSearchResult)
-              res.status(200).json({
-                iban,
-                bankCode,
-                bic: bankSearchResult[0].bic,
-                bankName: bankSearchResult[0].description,
-                country: countryCode,
-                responseCode: '200'
-              })
-                
-              break;
+                if(ibanLength != 22) res.json({message: 'IBAN Length is not correct', responseCode: '200.101'})
+        
+                // Get BankCode (Country Specific)
+                bankCode = iban.substring(4,12)
+                bankSearchResult = bankCodes.filter(it => it.bankCode === bankCode)
+                if(bankSearchResult == '') res.json({message: 'BIC not found for used IBAN', iban, bankCode, responseCode: '200.102'})
+                    console.log(bankSearchResult)
+                res.status(200).json({
+                    iban,
+                    bankCode,
+                    bic: bankSearchResult[0].bic,
+                    bankName: bankSearchResult[0].description,
+                    country: countryCode,
+                    responseCode: '200'
+                })
+                    
+                break;
 
             // AT - Austria
             case 'AT':
                 if(ibanLength != 20) res.json({message: 'IBAN Length is not correct', responseCode: '200.101'})  
 
                 // Get BankCode (Country Specific)
-              bankCode = iban.substring(4,9)
-              bankSearchResult = bankCodes.filter(it => it.bankCode === bankCode)
-              if(bankSearchResult == '') res.json({message: 'BIC not found for used IBAN', iban, bankCode, responseCode: '200.102'})
-              console.log(bankSearchResult)
+                bankCode = iban.substring(4,9)
+                bankSearchResult = bankCodes.filter(it => it.bankCode === bankCode)
+                if(bankSearchResult == '') res.json({message: 'BIC not found for used IBAN', iban, bankCode, responseCode: '200.102'})
+                console.log(bankSearchResult)
 
-              res.status(200).json({
-                iban,
-                bankCode,
-                bic: bankSearchResult[0].bic,
-                bankName: bankSearchResult[0].description,
-                country: countryCode,
-                responseCode: '200'
-              })
-                
-              break;
+                res.status(200).json({
+                    iban,
+                    bankCode,
+                    bic: bankSearchResult[0].bic,
+                    bankName: bankSearchResult[0].description,
+                    country: countryCode,
+                    responseCode: '200'
+                })
+                    
+                break;
 
-              default:
+              // CH - Switzerland
+            case 'CH':
+                  //
+            
+                  
+            // Fallback Case, if country is not supported
+            default:
                 res.json({message: 'No Valid Iban for allowed countries (DE, AT)'})
           }
     } catch (err) {
